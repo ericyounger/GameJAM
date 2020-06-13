fe6cf15ccbf07bb931b70aa22550a1dd1c5a7053
Tore Bergebakken <toberge@stud.ntnu.no>
2020-06-13 16:09:32 +0200

    Add basic platform movement

--- game.js
+++ game.js
@@ -5,21 +5,65 @@ var config = {
 	physics: {
 		default: "arcade",
 		arcade: {
-			gravity: { y: 200 },
+			gravity: { y: 300 },
 		},
 	},
 	scene: {
 		preload: preload,
 		create: create,
+		update: update
 	},
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
+let cursors;
+let stuff;
+let player;
+
+let jumping = false;
+
+console.log(game.input, this);
 
 function preload() {
-
+	this.load.image('icon', 'assets/icon.png');
+	this.load.image('grass', 'assets/grass.png');
 }
 
 function create() {
+	stuff = this.physics.add.staticGroup();
+	// floor
+	stuff.create(640, 720, 'grass').setScale(40, 1).refreshBody();
+	stuff.create(640, 600, 'grass').setScale(10, 1).refreshBody();
+
+	cursors = this.input.keyboard.createCursorKeys();
+
+	// player setup
+	player = this.physics.add.sprite(100, 450, 'icon');
+	player.setBounce(0.1);
+	player.setCollideWorldBounds(true);
+	player.body.setGravityY(300) // adds to global gravity
+
+	// add collision player-platform
+	this.physics.add.collider(player, stuff);
+}
+
+function update() {
+	// L/R movement
+	if (cursors.left.isDown) {
+		player.setVelocityX(-200);
+	} else if (cursors.right.isDown) {
+		player.setVelocityX(200);
+	} else {
+		player.setVelocityX(0);
+	}
+
+	// jumping
+	if (cursors.up.isDown && player.body.touching.down) {
+		player.setVelocityY(-400);
+		jumping = true;
+	} else if (cursors.down.isDown) {
+		player.setVelocityY(400);
+		
+	}
 
 }
